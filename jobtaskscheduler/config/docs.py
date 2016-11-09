4bf43d749484605b097516aacc064428b699eb37
Giovanni <giovanni@MacBook-Pro.station>
2016-11-09 22:29:45 +0100

    Added docs

--- jobtaskscheduler/config/docs.py
+++ jobtaskscheduler/config/docs.py
@@ -2,10 +2,11 @@
 Configuration for docs
 """
 
-# source_link = "https://github.com/[org_name]/jobtaskscheduler"
-# docs_base_url = "https://[org_name].github.io/jobtaskscheduler"
-# headline = "App that does everything"
-# sub_heading = "Yes, you got that right the first time, everything"
+source_link = "https://github.com/giovanni-codrotech/jobtaskscheduler"
+docs_base_url = "https://giovanni-codrotech.github.io/jobtaskscheduler"
+headline = "Job Task Scheduler"
+sub_heading = "A very simple scheulder for frappe using rq-schedule"
+
 
 def get_context(context):
-	context.brand_html = "Job Task Scheduler"
+    context.brand_html = "Job Task Scheduler"
